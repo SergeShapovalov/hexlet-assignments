dd8dcb30e48b988f249ab353eeb98ab0967a899e
Sergey Shapovalov <ssv@fozzy.com>
2024-04-10 21:41:33 +0200

    Fixed tests for CRUD exercise

--- rails-basics-ru/crud/db/seeds.rb
+++ rails-basics-ru/crud/db/seeds.rb
@@ -1,14 +1,12 @@
-# frozen_string_literal: true
-
 require 'faker'
 
-10.times do
-  Task.create(
-    name: Faker::Lorem.sentence(word_count: 3),
+25.times do
+  Task.create({
+    name: Faker::Lorem.sentence,
     description: Faker::Lorem.paragraph,
-    status: %i[in_progress failed].sample,
-    creator: Faker::ProgrammingLanguage.creator,
-    performer: %i[Jhon Tony ''].sample,
-    completed: %i[true false].sample
-  )
+    status: 'new',
+    creator: Faker::Name.name,
+    performer: Faker::Name.name,
+    completed: Faker::Boolean.boolean
+  })
 end
