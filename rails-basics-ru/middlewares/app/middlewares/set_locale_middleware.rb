766cde3fd416941b2316bdb448233d57a9e50ca0
Sergey Shapovalov <ssv@fozzy.com>
2024-05-06 13:08:11 +0200

    Added solution for Middlewares

--- rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
+++ rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
@@ -6,6 +6,18 @@ class SetLocaleMiddleware
   end
 
   # BEGIN
-  
+  def call(env)
+    I18n.locale = extract_locale_from_accept_language_header(env)
+    @app.call(env)
+  end
+
+  private
+
+  def extract_locale_from_accept_language_header(env)
+    return I18n.default_locale if env['HTTP_ACCEPT_LANGUAGE'].blank?
+
+    locale = env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    I18n.available_locales.include?(locale.to_sym) ? locale : I18n.default_locale
+  end
   # END
 end
