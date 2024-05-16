f32225213758f09f738c728caa37010cbbb89da0
Sergey Shapovalov <ssv@fozzy.com>
2024-05-16 12:42:11 +0200

    Real Rails ACL solution

--- rails-real-ru/acl/app/policies/post_policy.rb
+++ rails-real-ru/acl/app/policies/post_policy.rb
@@ -2,6 +2,33 @@
 
 class PostPolicy < ApplicationPolicy
   # BEGIN
-  
+  attr_reader :user, :post
+
+  def initialize(user, post)
+    super
+
+    @user = user
+    @post = post
+  end
+
+  def new?
+    user.present?
+  end
+
+  def create?
+    user.present?
+  end
+
+  def update?
+    user.present? && (user.admin? || user.id == post.author_id)
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.present? && user.admin?
+  end
   # END
 end
